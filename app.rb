970308260f46026a57669a96de96e7ba31edecb6
Dan Pelensky <dan@pelensky.com>
2017-07-16 09:24:34 +0100

    added config.ru file

--- app.rb
+++ app.rb
@@ -1,5 +1,10 @@
-require 'sinatra'
+require 'sinatra/base'
 
+class MyApp < Sinatra::Base
 get '/' do
   'Hello world!'
+end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
 end
---
+++ config.ru
@@ -0,0 +1,7 @@
+$LOAD_PATH << File.join(File.dirname(__FILE__))
+
+require 'rubygems'
+require File.join(File.dirname(__FILE__), './app.rb')
+
+run MyApp
+
